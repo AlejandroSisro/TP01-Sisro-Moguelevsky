874126463de6dae1faad0f49c4b55a1173d13fe5
50233806-create <50233806@est.ort.edu.ar>
2026-04-14 09:05:22 -0300

    Add files via upload

---
+++ Models/DatoFamilar.cs
@@ -0,0 +1,18 @@
+namespace TP01-Sisro-Moguelevsky
+{
+    public class DatoFamiliar
+    {
+      private string Nombre;
+      private string Apellido;
+      private string Parentesco;
+      private string Descripción;
+      public DatoFamiliar(string Nombre,string Apellido,string Parentesco,string Descripción)
+      {
+        this.Nombre=Nombre;
+        this.Apellido=Apellido;
+        this.Parentesco=Parentesco;
+        this.Descripción=Descripción;
+      } 
+
+    }
+}
---
+++ Models/Integrante.cs
@@ -0,0 +1,18 @@
+namespace TP01-Sisro-Moguelevsky
+{
+    public class integrante
+    {
+      private int dni;
+      private string Nombre;
+      private string fechaDnacimiento;
+      private string foto;
+      public integrante(int dni,string Nombre,string fechaDnacimiento,string foto)
+      {
+        this.dni=dni;
+        this.Nombre=Nombre;
+        this.fechaDnacimiento=fechaDnacimiento;
+        this.foto=foto;
+      } 
+      
+    }
+}
