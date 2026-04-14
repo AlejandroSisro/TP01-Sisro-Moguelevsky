b69b45594e6d6fbd769f9689091e274c7a749ba1
EZEQUIEL ILAN MOGUELEVSKY <50233806@ort>
2026-04-14 08:08:02 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP01_Sisro_Moguelevsky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
