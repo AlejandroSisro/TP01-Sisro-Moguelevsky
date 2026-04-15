d4904d3c6895bf9ee7a440998002ded1ea5b6dac
AlejandroSisro <50434059@est.ort.edu.ar>
2026-04-15 08:57:46 -0300

    Update Grupo.cs

--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -2,28 +2,28 @@ namespace TP01-Sisro-Moguelevsky
 {
     public class Grupo
     {
-    private Dictionary <int, Integrante> dicAlumnos = new Dictionary <int, Integrante>();
-
-    public Grupo()
-    {
-    Dictionary <int, Alumno> dicAlumnos = new Dictionary <int, Alumno>();
-    } 
-    public void CargaDatosManual()
-    {
-        Integrante Alumno1 = new Integrante(50434049, "Alejandro", "21/06/2010", "Ale");
-        Integrante Alumno2 = new Integrante(50233806, "Ezequiel", "15/03/2010", "Echu");
-        dicAlumnos.Add(Alumno1);
-        dicAlumnos.Add(Alumno2);
-    }
-       public void DevolverIntegrantes()
-    {
-          foreach(Dictionary objDic in Integrante.Values)
+        private Dictionary <int, Integrante> dicAlumnos = new Dictionary <int, Integrante>();
+    
+        public Grupo()
+        {
+        Dictionary <int, Alumno> dicAlumnos = new Dictionary <int, Alumno>();
+        } 
+        public void CargaDatosManual()
+        {
+            Integrante Alumno1 = new Integrante(50434049, "Alejandro", "21/06/2010", "Ale");
+            Integrante Alumno2 = new Integrante(50233806, "Ezequiel", "15/03/2010", "Echu");
+            dicAlumnos.Add(Alumno1);
+            dicAlumnos.Add(Alumno2);
+        }
+        public void DevolverIntegrantes()
+        {
+            foreach(Dictionary objDic in Integrante.Values)
             {
                 Console.WriteLine(objDic.Alumno1); 
                 Console.WriteLine(objDic.Alumno2); 
             }
-        
-    }
+            
+        }
     }
    
 }
