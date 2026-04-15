13f807984a5217603da823693f0607028f7c6332
50233806-create <50233806@est.ort.edu.ar>
2026-04-15 08:08:12 -0300

    Refactor Integrante class and add new properties
    Refactor Integrante class to improve structure and add Familiares and Intereses lists.

--- Models/Integrante.cs
+++ Models/Integrante.cs
@@ -1,17 +1,21 @@
 namespace TP01-Sisro-Moguelevsky
 {
-    public class integrante
+    public class Integrante
     {
       private int dni;
       private string Nombre;
       private string fechaDnacimiento;
       private string foto;
-      public integrante(int dni,string Nombre,string fechaDnacimiento,string foto)
+      private List<string> Familiares;
+      private List<string> Intereses;
+      public  Integrante(int dni,string Nombre,string fechaDnacimiento,string foto)
       {
         this.dni=dni;
         this.Nombre=Nombre;
         this.fechaDnacimiento=fechaDnacimiento;
         this.foto=foto;
+        List<string> Familiares = new List<string>;
+        List<string>  Intereses = new List<string>;
       } 
       
     }
