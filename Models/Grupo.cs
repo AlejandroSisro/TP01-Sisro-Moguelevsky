0820a8c1470f2aa26269bc3a74cd39aa119b9677
AlejandroSisro <50434059@est.ort.edu.ar>
2026-04-22 08:06:33 -0300

    Refactor Grupo class and variable naming
    Refactor Grupo class to use 'integrantes' instead of 'Integrantes' for consistency and fix potential issues with dictionary usage.

--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -2,29 +2,29 @@ namespace TP01-Sisro-Moguelevsky
 {
     public class Grupo
     {
-        private Dictionary <int, Integrante> Integrantes = new Dictionary <int, Integrante>();
+        private Dictionary <int, Integrante> integrantes = new Dictionary <int, Integrante>();
     
         public Grupo()
         {
-        Dictionary <int, Alumno> Integrantes = new Dictionary <int, Alumno>();
+        Dictionary <int, Alumno> integrantes = new Dictionary <int, Alumno>();
         CargaDatosManual();
         } 
         private void CargaDatosManual()
         {
             Integrante Alumno1 = new Integrante(50434049, "Alejandro", "21/06/2010", "Ale.jpg");
             Integrante Alumno2 = new Integrante(50233806, "Ezequiel", "15/03/2010", "Echu.jpg");
-            Integrantes.Add(Alumno1);
-            Integrantes.Add(Alumno2);
+            integrantes.Add(Alumno1);
+            integrantes.Add(Alumno2);
         }
         public Dictionary<int,Integrante> DevolverIntegrantes()
         {
-            return Integrantes;
+            return integrantes;
         }
         public Integrante GetIntegrantes(int dni)
         {
-            if(Integrantes.ContainsKey(dni) = true)
+            if(integrantes.ContainsKey(dni) = true)
             {
-                return Integrantes[dni];
+                return integrantes[dni];
             }
             else
             {
